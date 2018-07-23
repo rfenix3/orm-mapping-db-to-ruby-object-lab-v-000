dde45fe3c14677886f3df51735b5c4ef0d4eaab9
Richard Fenix <richard.fenix@gmail.com>
2018-07-23 05:20:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
     sql = "SELECT * from students where name = ?"
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
   
   def save
